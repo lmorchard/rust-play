2731c0d605fe22fe4cfab099d21b6321fe772a82
Les Orchard <me@lmorchard.com>
2017-12-19 17:25:43 -0500

    And now I know how to propagate errors

---
+++ ch09/propagating/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
+fn main() {
+    let username = match read_username_from_file() {
+        Ok(s) => s.trim().to_string(),
+        Err(e) => panic!("So much for that username {}", e),
+    };
+    println!("Hello, {:?}", username);
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e)
+    }
+}
