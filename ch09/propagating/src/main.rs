190b5cdbea6cf9c76ce1eb794b60022f2153a18b
Les Orchard <me@lmorchard.com>
2017-12-20 15:48:37 -0500

    Error handling can be even shorter

--- ch09/propagating/src/main.rs
+++ ch09/propagating/src/main.rs
@@ -3,11 +3,28 @@ use std::io::Read;
 use std::fs::File;
 
 fn main() {
-    let username = match read_username_from_file() {
+    example1();
+}
+
+fn example1() {
+    let username = match read_username_from_file_shortest() {
         Ok(s) => s.trim().to_string(),
         Err(e) => panic!("So much for that username {}", e),
     };
     println!("Hello, {:?}", username);
+}
+
+fn read_username_from_file_shortest() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file_shorter() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
